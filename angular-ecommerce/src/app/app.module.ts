a705558fb2e1a48812589f853d38e0bd95affae0
mustafa <mstfdlk01@gmail.com>
2024-02-20 00:37:54 +0300

    Developer account created at okta. App configuraiton is setted for OpenID Connect. Okta sdk packages installed. Login status componet created.

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -15,8 +15,23 @@ import { CartStatusComponent } from './component/cart-status/cart-status.compone
 import { CartDetailsComponent } from './component/cart-details/cart-details.component';
 import { CheckoutComponent } from './component/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './component/login/login.component';
+import { LoginStatusComponent } from './component/login-status/login-status.component';
+import{
+  OktaAuthModule,
+  OktaCallbackComponent,
+  OKTA_CONFIG
+} from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import myAppConfig from './config/my-app-config';
+
+const oktaConfig= myAppConfig.oidc;
+
+const oktaAuth= new OktaAuth(oktaConfig);
 
 const routes: Routes=[ 
+  {path:'login/callback', component:OktaCallbackComponent},
+  {path:'login', component:LoginComponent},
   {path:'checkout', component:CheckoutComponent},
   {path:'cart-details', component:CartDetailsComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
@@ -39,16 +54,19 @@ const routes: Routes=[
     ProductDetailsComponent,
     CartStatusComponent,
     CartDetailsComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    LoginComponent,
+    LoginStatusComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
     NgbModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    OktaAuthModule
   ],
-  providers: [ProductService],
+  providers: [ProductService,{provide: OKTA_CONFIG, useValue:{oktaAuth}}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc:{
+        clientId:'0oaf8k447zzYXlEPs5d7',
+        issuer:'https://dev-33088881.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid','profile','email']
+        
+    }
+}
