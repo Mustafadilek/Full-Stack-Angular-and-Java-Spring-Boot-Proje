e3b251702e4b490d9cfe65e5307ae94e08098523
mustafa <mstfdlk01@gmail.com>
2024-02-20 22:10:50 +0300

    Keeping track of logged in user's email with web browser storage. OrderHistory service, class, component created. Protected route for order history component defined.

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -26,6 +26,7 @@ import{
 import { OktaAuth } from '@okta/okta-auth-js';
 import myAppConfig from './config/my-app-config';
 import { MembersPageComponent } from './component/members-page/members-page.component';
+import { OrderHistoryComponent } from './component/order-history/order-history.component';
 
 const oktaConfig= myAppConfig.oidc;
 
@@ -39,6 +40,8 @@ function sendToLoginPage(oktaAuth: OktaAuth, injector: Injector){
 }
 
 const routes: Routes=[ 
+  {path:'order-history',component:OrderHistoryComponent, canActivate:[OktaAuthGuard],
+  data:{onAuthRequired:sendToLoginPage}},
   {path: 'members', component: MembersPageComponent, canActivate:[OktaAuthGuard],
                     data:{onAuthRequired:sendToLoginPage}} ,
   {path:'login/callback', component:OktaCallbackComponent},
@@ -68,7 +71,8 @@ const routes: Routes=[
     CheckoutComponent,
     LoginComponent,
     LoginStatusComponent,
-    MembersPageComponent
+    MembersPageComponent,
+    OrderHistoryComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
--- angular-ecommerce/src/app/component/checkout/checkout.component.ts
+++ angular-ecommerce/src/app/component/checkout/checkout.component.ts
@@ -30,6 +30,8 @@ export class CheckoutComponent implements OnInit {
   shippingAddressStates:State[]=[];
   billingAddressStates:State[]=[];
   
+  storage: Storage=sessionStorage;
+
   constructor( private formBuilder: FormBuilder,
                private luv2ShopFormService:Luv2ShopFormService,
                private cartService: CartService,
@@ -42,6 +44,10 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
 
     this.reviewCartDetails();
+
+    // retrieve the user's email address from browser storage
+    const theEmail= JSON.parse(this.storage.getItem('userEmail')!);
+
     
     this.checkoutFormGroup= this.formBuilder.group({
       customer: this.formBuilder.group({
@@ -51,7 +57,7 @@ export class CheckoutComponent implements OnInit {
         lastName:new FormControl('',[ Validators.required,
                                       Validators.minLength(2),   
                                       CustomValidators.notOnlyWhitespace]),
-        email:new FormControl('',
+        email:new FormControl(theEmail,
                               [Validators.required,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
